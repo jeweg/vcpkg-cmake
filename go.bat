77ce37635b0fb1be87a8f39893981ee66be54e31
Jens Weggemann <jensweh@gmail.com>
2019-11-19 00:47:45 +0100

    Vcpkg was mistakenly still a submodule. Removed it.

--- go.bat
+++ go.bat
@@ -1,7 +1,9 @@
+mkdir build
 pushd build
-del /Q /F *
-cmake -G "Visual Studio 16 2019" -A x64 ..
-cmake --build .
+if not errorlevel 1 del /Q /F * 
+if not errorlevel 1 cmake -G "Visual Studio 16 2019" -A x64 ..
+if not errorlevel 1 cmake --build .
 popd
-call build\Debug\main.exe
+if not errorlevel 1 call build\Debug\main.exe
+
 rem pause
